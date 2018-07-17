1f8bbf6c8b47f464c1139bcf9a4260cfdab32776
ehauckdo <ehauckdo@uwaterloo.ca>
2018-07-17 05:44:23 -0400

    First half of rollout

--- Node.py
+++ Node.py
@@ -1,14 +1,21 @@
+import random
+
+operators = ["+", "-"]
+terminals = [1, "x"]
+max_height = 5
+
 class Node(object):
     
-    operators = ["+", "-"]
-    terminals = [1, "x"]
-
     def __init__(self):
-        self.value = None
+        self.value = []
+        self.terminals = 0
+        self.operators = 0
+
         self.visits = 0;
         self.reward = 0;
         self.fullyExpanded = False
         self.children = None;
+        self.rollOut()
 
     def iteration(self):
         node = treePolicy()
@@ -19,6 +26,25 @@ class Node(object):
 
     def rollOut(self):
         # execute rollout and return Reward
+        rollout_stack = list(self.value)
+        rollout_terminals = self.terminals
+        rollout_operators = self.operators
+
+        while len(rollout_stack) < max_height:
+            if rollout_operators >= max_height/2:
+                self.pushTerminal(rollout_stack)
+                rollout_terminals += 1
+            else:
+                term = self.pushAny(rollout_stack)
+                if term in terminals:
+                    rollout_terminals += 1
+                else:
+                    rollout_operators += 1
+
+            print("rollout_stack: ", rollout_stack) 
+            print("number of terminals", rollout_terminals) 
+            print("number of operators: ", rollout_operators) 
+
         return 0
 
     def backup(self, node, reward):
@@ -28,3 +54,20 @@ class Node(object):
     def expand(self):
         if self.fullyExpanded != True:
             # expand the tree
+            return
+
+    def pushTerminal(self, stack):
+        term = random.choice(terminals)
+        stack.append(term)
+        return term
+
+    def pushOperator(self, stack):
+        term = random.choice(operators)
+        stack.append(term)
+        return term
+
+    def pushAny(self, stack):
+        candidates = operators + terminals 
+        term = random.choice(candidates)
+        stack.append(term)
+        return term
