5090debdf05ea3d9bb9423fae8888b784faf7a32
ehauckdo <ehauckdo@uwaterloo.ca>
2018-11-02 15:44:14 +0900

    Improved output

--- MCTS/MCTS.py
+++ MCTS/MCTS.py
@@ -4,8 +4,6 @@ import time
 from SearchNode import SearchNode
 from SolutionHandler import SolutionHandler
 
-#logging.basicConfig(filename='log',level=logging.INFO, filemode='w')
-
 class MCTS(object):
 
     def __init__(self, handler=SolutionHandler(), iterations=50000):
@@ -16,11 +14,3 @@ class MCTS(object):
     def run(self):
         for i in range(self.iterations):
             self.root.iteration()
- 
-        if len(self.handler.zeroErrorSolution) > 0:
-            for sol in self.handler.zeroErrorSolution:
-                print("Solution option: \n"+"\n".join(self.handler.printComponents(sol)))
-        #else:
-        #    result = self.root.bestBranch()
-        #    print("Best brach: \n"+"\n".join(self.handler.printComponents(result)))
-
